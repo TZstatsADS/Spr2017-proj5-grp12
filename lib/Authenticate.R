9875b2c2f7f6a56fd67dbbfb62b995778b3e5e26
helen-lo <kl2739@barnard.edu>
2017-04-24 19:56:05 -0400

    authentication key for twitter APIs

---
+++ lib/Authenticate.R
@@ -0,0 +1,15 @@
+#Authenticate.R
+library(twitteR)
+#api_key<- "xx"
+#api_secret<- "xx"
+#access_token<- "xx"
+#access_token_secret<- "xx"
+#setup_twitter_oauth(api_key,api_secret,access_token,access_token_secret)
+
+
+api_key <- "izRunXa7pXEAwSjUML1n99Ef0"
+api_secret <- "vwTTcuFOxeqM9VhEiiDJdLtVHXVdGXxyAbKEnow7kvqzww88JI"
+access_token <- "1950002257-WG5hnJYTK7wYezHQUbMXnAsIqOffhzfFAoL5NlE"
+access_token_secret <- "4OdiqIR2W4kdJbcnEME2BmkhYnpQvj36QIQaAnNDYR20H"
+
+setup_twitter_oauth(api_key,api_secret,access_token,access_token_secret)
